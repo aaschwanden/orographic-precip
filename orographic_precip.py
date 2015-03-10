c24698c579d1f57aa850159165776161b5e76143
Andy Aschwanden <aaschwanden@alaska.edu>
2015-03-09 20:16:07 -0800

    my first nosetest

--- orographic_precip.py
+++ orographic_precip.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Copyright (C) 2015 Andy Aschwanden
 
+# nosetests orographic_precipy.py
+
 import numpy as np
 from scipy import integrate
 
@@ -10,7 +12,6 @@ nx =30e2
 dx = 30e3
 
 gamma = -6.5 * 1e-3
-e0 =
 
 # Set up domain
 x = np.linspace(0, L, nx+1)
@@ -19,14 +20,30 @@ x = np.linspace(0, L, nx+1)
 def Ts(x):
     return TsL - gamma * z
 
-def esat(Ts):
-    return e0*exp((a*Ts)/(b+Ts))
+def esat(T):
+    '''
+    Returns water vapor saturation pressure in Pa at temperature T [Celsius] according to
+    August-Roche-Magnus formula
+    '''
+    
+    e0 = 610.94
+    a = 17.625
+    b = 243.04
+    
+    return e0*np.exp((a*T)/(b+T))
+
+def esat_test():
+    
+    T = np.array([-5, 0, 5], dtype=np.float64)
+
+    assert np.max(np.fabs(esat(T) - np.array([ 421.9082468, 610.94, 871.55954949]))) < 1e-8
+
 
 def gaussian_smoothing(t, x, dx):
     return exp(-((x-t)/dx)**2)
 
-def minus_nabla_F:
-    return alpha0 _ alpha1*vbar*dzdx*esat
+def minus_nabla_F():
+    return alpha0 - alpha1*vbar*dzdx*esat
 
 def integrand():
     return minus_nabla_F *gaussian_smoothing
@@ -34,5 +51,5 @@ def integrand():
 def expint(x, dx):
     return quad(gaussian_smoothing, x, Inf, args=(x, dx))[0]
 
-vec_expint = vectorize(expint)
+vec_expint = np.vectorize(expint)
 
